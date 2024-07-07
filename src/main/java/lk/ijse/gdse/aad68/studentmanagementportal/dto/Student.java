049a3feae6396763eca908fe3a177eb4e97578b1
ThanuraSilva <thanurasilva1989@gmail.com>
2024-07-07 12:41:29 +0530

    Process JSON type string

--- src/main/java/lk/ijse/gdse/aad68/studentmanagementportal/dto/Student.java
+++ src/main/java/lk/ijse/gdse/aad68/studentmanagementportal/dto/Student.java
@@ -1,4 +1,4 @@
-package lk.ijse.gdse.aad68.studentmanagementportal.controller;
+package lk.ijse.gdse.aad68.studentmanagementportal.dto;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -6,13 +6,22 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/student")
 public class Student extends HttpServlet {
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //Todo: Save student
+        if(req.getContentType() == null || !req.getContentType().toLowerCase().startsWith("application/json")){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+        BufferedReader reader = req.getReader();
+        StringBuilder sb = new StringBuilder();
+        reader.lines().forEach(line -> sb.append(line).append("\n"));
+        System.out.println(sb);
+
     }
 
     @Override
